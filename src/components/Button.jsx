969c0b8b14fc0c53eb230ba3deaab49453986a5d
AniketParmar <80318654+aniketparmar29@users.noreply.github.com>
2024-03-15 18:44:01 +0530

    ASD

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+const Button = ({ className, px, white, onClick, href, children }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}  `;
+  const spanClasses = `relative z-10`;
+  const renderButton = () => (
+    <button onClick={onClick} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+  const renderLink = () => (
+    <a href={href} onClick={onClick} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+  return href ? renderLink() : renderButton();
+};
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,81 @@
+import { useLocation } from "react-router-dom";
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HambugerMenu } from "./design/Header";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { useState } from "react";
+const Header = () => {
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+  const toggle = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+  const handleClick = () => {
+    if (!openNavigation) return;
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
+  return (
+    <>
+      <div
+        className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:border-n-8/90 lg:backdrop-blur-sm ${
+          openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+        }`}
+      >
+        <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+          <a className="block w-[12rem] xl:mr-8" href="#hero">
+            <img src={brainwave} width={190} height={40} alt="BrainWave" />
+          </a>
+          <nav
+            className={`${
+              openNavigation ? "flex" : "hidden"
+            } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent `}
+          >
+            <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+              {navigation.map((el) => (
+                <a
+                  key={el.id}
+                  href={el.url}
+                  onClick={handleClick}
+                  className={`block relative font-code text-2xl uppercase text-n-1 transition-color hover:text-color-1 ${
+                    el.onlyMobile ? "lg:hidden" : ""
+                  } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                    el.url == pathname.hash
+                      ? "z-2 lg:text-n-1"
+                      : "lg:text-n-1/50"
+                  } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                >
+                  {el.title}
+                </a>
+              ))}
+            </div>
+            <HambugerMenu />
+          </nav>
+
+          <a
+            href="#singUp"
+            className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+          >
+            New Account
+          </a>
+          <Button className={`hidden lg:flex`} href="#login">
+            Sing In
+          </Button>
+          <Button onClick={toggle} className={`ml-auto lg:hidden`} px={`px-3`}>
+            <MenuSvg openNavigation={openNavigation} />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
