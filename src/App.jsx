83e969b5307d7961a1355b5226b30e69092955e7
AniketParmar <80318654+aniketparmar29@users.noreply.github.com>
2024-03-21 13:24:48 +0530

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -8,6 +9,7 @@ const App = () => {
       <div className="p-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <div>
         <ButtonGradient />
